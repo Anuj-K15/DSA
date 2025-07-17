f8c55bbf4ceae19d087904674177ab6572da3d69
Anuj Karambalkar <anujkarambalkar1504@gmail.com>
2025-07-17 23:55:04 +0530

    Add MaxSumSubArray class with methods for brute force and prefix sum approaches to find maximum sum of subarrays

---
+++ Arrays 2/MaxSumSubArray.java
@@ -0,0 +1,53 @@
+public class MaxSumSubArray {
+    public static void subArraysMaxSumBruteForce(int numbers[]) {
+        // int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for (int  i = 0; i < numbers.length; i++) {
+            int start = i;
+            for (int j = i; j < numbers.length; j++) {
+                int end = j;
+                int currSum = 0; // Reset current sum for each subarray
+                for (int k = start; k <= end; k++) {
+                    currSum += numbers[k];
+                }
+                System.out.println(currSum);
+                if (maxSum < currSum) {
+                    maxSum = currSum; // Update maxSum if current sum is greater
+                }
+            }
+        }
+        System.out.println("Maximum sum of subarrays: " + maxSum);
+    }   
+
+    public static void subArraysMaxSumPrefixArray(int numbers[]) {
+        // int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        int prefix[] = new int [numbers.length];
+
+        prefix[0] = numbers[0];
+        //calculate prefix sum
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i - 1] + numbers[i];
+        }
+
+        for (int  i = 0; i < numbers.length; i++) {
+            int start = i;
+            for (int j = i; j < numbers.length; j++) {
+                int end = j;
+                int currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1]; // Reset current sum for each subarray
+                
+                if (maxSum < currSum) {
+                    maxSum = currSum; // Update maxSum if current sum is greater
+                }
+            }
+        }
+        System.out.println("Maximum sum of subarrays: " + maxSum);
+    }   
+    
+    public static void main(String[] args) {
+        int numbers[] = {2, -1, -5, 4, 3};
+        subArraysMaxSumBruteForce(numbers);
+        subArraysMaxSumPrefixArray(numbers);
+    }
+}
