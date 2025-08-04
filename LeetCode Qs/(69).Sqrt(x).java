958c17390f15aef1bbb89585c64f55c090778688
Anuj Karambalkar <anujkarambalkar1504@gmail.com>
2025-08-05 01:41:10 +0530

    Add initial implementation of Sqrt algorithm using binary search

---
+++ LeetCode Qs/(69).Sqrt(x).java
@@ -0,0 +1,15 @@
+// class Solution {
+//     public int mySqrt(int x) {
+//         long low = 1, high = x;
+//         while (low <= high) {
+//             long mid = (low + high) / 2;
+//             long value = mid * mid;
+//             if (value <= x) {
+//                 low = mid + 1;
+//             } else {
+//                 high = mid - 1;
+//             }
+//         }
+//         return (int) high;
+//     }
+// }
