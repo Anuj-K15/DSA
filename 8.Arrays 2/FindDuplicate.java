25a7d974c953e5c1214a2c68c2a5788eff235a54
Anuj Karambalkar <anujkarambalkar1504@gmail.com>
2025-08-07 01:03:10 +0530

    feat: add solutions for LeetCode problems 136 and 287
    Implement efficient solutions for Single Number (136) using XOR operation
    and Find the Duplicate Number (287) using Floyd's cycle detection algorithm

---
+++ 8.Arrays 2/FindDuplicate.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author anujk
+ */
+public class FindDuplicate {
+    public static int findDuplicate(int[] nums) {
+        int slow = nums[0], fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+    public static void main(String[] args) {
+        int nums[] = {1, 3, 4, 2, 2};
+        System.out.println(findDuplicate(nums));
+    }
+}
