4413bb171551f638752bb892c5f131a35ec16acb
Anuj Karambalkar <anujkarambalkar1504@gmail.com>
2025-08-12 00:55:42 +0530

    feat: add solutions for three algorithm problems
    Add implementations for:
    1. Product of array except self (LeetCode 238)
    2. Maximum sum subarray of size K (GFG)
    3. Longest substring without repeating characters (LeetCode 3)
    
    All solutions include time and space complexity analysis.

---
+++ GFG Qs/MaxSumSubarrayOfSizeK.java
@@ -0,0 +1,23 @@
+// class Solution {
+//     public int maximumSumSubarray(int[] arr, int k) {
+//         // Code here
+//         int i = 0;
+//         int j = 0;
+//         int sum = 0;
+//         int maxSum = Integer.MIN_VALUE;
+//         while (j < arr.length) {
+//             sum = sum + arr[j];
+//             if (j-i+1 < k) {
+//                 j++;
+//             } else if (j-i+1 == k) {
+//                 maxSum = Math.max(sum, maxSum);
+//                 sum = sum - arr[i];
+//                 i++;
+//                 j++;
+//             }
+//         }
+//         return maxSum;
+//     }
+// }
+// // Time Complexity: O(n)
+// // Space Complexity: O(1)
