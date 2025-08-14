5018ba30913dbe661778eeeeb67da0d940d0171e
Anuj Karambalkar <anujkarambalkar1504@gmail.com>
2025-08-15 00:38:00 +0530

    feat: add CountVowels program to count vowels in a string

---
+++ 11.Strings/CountVowels.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class CountVowels {
+
+    public static void main(String[] args) {
+        String str = "ApnaCollege".replace("l", "");
+        System.out.println(str);
+
+    }
+}
+// Time Complexity: O(n)
+// Space Complexity: O(1)
