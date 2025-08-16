cc3aab893b8fe80c7fb0350a9dbfca0219c59ab4
Anuj Karambalkar <anujkarambalkar1504@gmail.com>
2025-08-16 21:56:13 +0530

    feat: add solutions for LeetCode problems 1323 and 14
    Add Java solutions for Maximum 69 Number and Longest Common Prefix problems

---
+++ LeetCode Qs/(14).LongestCommonPrefix.java
@@ -0,0 +1,15 @@
+// class Solution {
+//     public String longestCommonPrefix(String[] strs) {
+//         String prefix = strs[0];
+//         if (strs == null || strs.length == 0) return "";
+//         for (int i = 1; i < strs.length; i++) {
+//             while (!strs[i].startsWith(prefix)) {
+//                 prefix = prefix.substring(0, prefix.length() - 1);
+//                 if (prefix == "") return "";
+//             }
+//         }
+//         return prefix;
+//     }
+// }
+// // Time Complexity: O(n)
+// // Space Complexity: O(1)
