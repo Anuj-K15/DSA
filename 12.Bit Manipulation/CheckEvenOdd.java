6dda7b846997ebdea2a3dc550f09f703ef4fa796
Anuj Karambalkar <anujkarambalkar1504@gmail.com>
2025-08-19 22:19:20 +0530

    feat: add bit manipulation utility to check even/odd numbers

---
+++ 12.Bit Manipulation/CheckEvenOdd.java
@@ -0,0 +1,16 @@
+public class CheckEvenOdd {
+    public static void oddEven(int n) {
+        int bitMask = 1;
+        if ((n & bitMask) == 0) {
+            System.out.println("Even Number");
+        }
+        else {
+            System.out.println("Odd Number");
+        }
+    }
+    public static void main(String[] args) {
+        oddEven(10);
+        oddEven(11);
+        oddEven(12);
+    }
+}
