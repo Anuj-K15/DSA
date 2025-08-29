1de97a25f4928b5a79a7f9cfca3c48aa992a0dfa
Anuj Karambalkar <anujkarambalkar1504@gmail.com>
2025-08-30 00:29:37 +0530

    feat(Bit Manipulation): add new utility methods to IthBit class
    Add isPowerOfTwo, countSetBits, and fastExponentiation methods to provide additional bit manipulation utilities. Also includes formatting improvements and new test cases in main method.
    
    feat: add LeetCode flower game solution
    
    Implement solution for Alice and Bob playing flower game problem (LeetCode 3021).

--- 12.Bit Manipulation/IthBit.java
+++ 12.Bit Manipulation/IthBit.java
@@ -1,34 +1,68 @@
+
 public class IthBit {
+
     public static int getIthBit(int n, int i) {
-        int bitMask = 1<<i;
-        if((n & bitMask) == 0) {
+        int bitMask = 1 << i;
+        if ((n & bitMask) == 0) {
             return 0;
         } else {
             return 1;
         }
     }
+
     public static int setIthBit(int n, int i) {
-        int bitMask = 1<<i;
+        int bitMask = 1 << i;
         return n | bitMask;
     }
+
     public static int clearIthBit(int n, int i) {
-        int bitMask = ~(1<<i);
+        int bitMask = ~(1 << i);
         return bitMask & n;
     }
+
     public static int updateIthBit(int n, int i, int newBit) {
         n = clearIthBit(n, i);
-        int bitMask = newBit<<i;
+        int bitMask = newBit << i;
         return bitMask | n;
     }
+
     public static int clearLastIthBit(int n, int i) {
-        int bitMask = ((~0)<<i);
+        int bitMask = ((~0) << i);
         return bitMask & n;
     }
+
     public static int clearRangeIthBit(int n, int i, int j) {
-        int a = ((~0)<<(j+1));
-        int b = (1<<i)-1;
-        int bitMask = a|b;
+        int a = ((~0) << (j + 1));
+        int b = (1 << i) - 1;
+        int bitMask = a | b;
         return bitMask & n;
+    }
+
+    public static boolean isPowerOfTwo(int n) {
+        return (n & (n - 1)) == 0;
+    }
+
+    public static int countSetBits(int n) {
+        int count = 0;
+        while (n > 0) {
+            if ((n & 1) != 0) {
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+
+    public static int fastExponentiation(int a, int n) {
+        int ans = 1;
+        while (n > 0) {
+            if ((n & 1) != 0) {
+                ans = ans * a;
+            }
+            a = a * a;
+            n = n >> 1;
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
@@ -38,5 +72,9 @@ public class IthBit {
         System.out.println(updateIthBit(10, 2, 1));
         System.out.println(clearLastIthBit(15, 2));
         System.out.println(clearRangeIthBit(10, 2, 4));
+        System.out.println(isPowerOfTwo(16));
+        System.out.println(isPowerOfTwo(15));
+        System.out.println(countSetBits(10));
+        System.out.println(fastExponentiation(2, 5));
     }
 }
---
+++ LeetCode Qs/(3021).AliceAndBobPlayingFlowerGame.java
@@ -0,0 +1,7 @@
+// class Solution {
+//     public long flowerGame(int n, int m) {
+//         return (long)((n+1)/2) * (m/2) + (long)((m+1)/2) * (n/2);
+//     }
+// }
+// // Time Complexity: O(1)
+// // Space Complexity: O(1)
