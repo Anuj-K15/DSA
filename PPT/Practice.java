5212628266573e77e0b72a6cbe3135f25b1865c2
Anuj Karambalkar <anujkarambalkar1504@gmail.com>
2025-09-09 00:42:12 +0530

    feat: add array diagonal printing and LeetCode solution
    Add functionality to print diagonal elements of a 2D array in Z shape
    Include LeetCode solution for converting integer to sum of two non-zero integers

--- PPT/Practice.java
+++ PPT/Practice.java
@@ -1,11 +1,14 @@
-
+import java.util.Scanner;
 public class Practice {
-
     public static void main(String[] args) {
-        byte x = 127;
-        x++;
-        x++;
-        System.out.println(x);
+        int [][] arr = {{1,2,3,4},{4,5,6,7},{7,8,9,3}};
+        // print diagonal elements ( Z shape )
+        for (int i=0; i<arr.length; i++) {
+            System.out.print(arr[0][i] + " ");
+        for (int j=i; j<arr.length; j++) {
+            System.out.print(arr[j][arr.length-1-i] + " ");
+        }
+        }
+        // System.out.println();
     }
-
 }
---
+++ PPT/tempCodeRunnerFile.java
@@ -0,0 +1,3 @@
+for (int j=0; j<arr.length; j++) {
+                System.out.print(arr[0][j] + " ");
+         
